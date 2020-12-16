c8d9182866674e3ba87e64c62b6f3f8f55f06bf7
jayden-mac <737043980@qq.com>
2020-12-16 23:23:33 +0800

    更新

--- client/main.go
+++ client/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/zzsds/trade"
@@ -24,8 +26,30 @@ func main() {
 		time.Sleep(2 * time.Second)
 		m.Resume()
 	}()
+
 	t.Add(m)
-	t.Run()
+	go t.Run()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		// 输出到STDOUT展示处理已经开始
+		fmt.Fprint(os.Stdout, "processing request\n")
+		// 通过select监听多个channel
+		select {
+		case <-time.After(2 * time.Second):
+			// 如果两秒后接受到了一个消息后，意味请求已经处理完成
+			// 我们写入"request processed"作为响应
+			w.Write([]byte("request processed"))
+		case <-ctx.Done():
+
+			// 如果处理完成前取消了，在STDERR中记录请求被取消的消息
+			fmt.Fprint(os.Stderr, "request cancelled\n")
+		}
+	})
+	http.HandleFunc("/hello", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write([]byte("hello request"))
+	})
+	// 创建一个监听8000端口的服务器
+	http.ListenAndServe(":8000", nil)
 }
 
 func queueTest() {
